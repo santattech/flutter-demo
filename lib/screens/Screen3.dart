326f1626b9d12ade78c5915d7ed689981e293482
Santanu Bhattacharya <santattech@gmail.com>
2023-05-27 17:49:25 +0530

    feat: design travel time and start end trip button

--- lib/screens/Screen3.dart
+++ lib/screens/Screen3.dart
@@ -4,27 +4,61 @@ class Screen3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Screen 3"),
-        ),
-        body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.all(5),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 63, 218, 99),
-                      foregroundColor: Colors.white,
-                    ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text('Go Back'),
+      appBar: AppBar(
+        title: const Text("Speedometer"),
+      ),
+      body: Column(children: <Widget>[
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.all(20),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 45, 215, 245),
+                    foregroundColor: Colors.black,
                   ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Start Trip'),
                 ),
-              ]),
-        ));
+              ),
+              Container(
+                margin: const EdgeInsets.all(5),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 45, 215, 245),
+                    foregroundColor: Colors.black,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('End Trip'),
+                ),
+              )
+            ]),
+        Container(
+            decoration: BoxDecoration(
+              border: Border.all(width: 2, color: Colors.black12),
+              color: Colors.black,
+            ),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.all(20),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                      color: Colors.black,
+                    ),
+                    child: const Text(
+                      '01:30:25',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  )
+                ]))
+      ]),
+    );
   }
 }
