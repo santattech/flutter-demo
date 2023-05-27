03233c65bf58e48e2a3fcc66db71b4d9126cd9c4
Santanu Bhattacharya <santattech@gmail.com>
2023-05-27 18:12:58 +0530

    feat: move the trip button to functions

--- lib/screens/Screen3.dart
+++ lib/screens/Screen3.dart
@@ -8,57 +8,56 @@ class Screen3 extends StatelessWidget {
         title: const Text("Speedometer"),
       ),
       body: Column(children: <Widget>[
-        Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.all(20),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 45, 215, 245),
-                    foregroundColor: Colors.black,
-                  ),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text('Start Trip'),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.all(5),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 45, 215, 245),
-                    foregroundColor: Colors.black,
-                  ),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text('End Trip'),
-                ),
-              )
-            ]),
         Container(
             decoration: BoxDecoration(
-              border: Border.all(width: 2, color: Colors.black12),
+              border: Border.all(width: 2, color: Colors.yellow),
               color: Colors.black,
             ),
             child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.all(20),
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(15)),
-                      color: Colors.black,
-                    ),
-                    child: const Text(
-                      '01:30:25',
-                      style: TextStyle(fontSize: 20, color: Colors.white),
-                    ),
-                  )
-                ]))
+                  _tripButtons('Start Trip', context),
+                  _tripButtons('End Trip', context),
+                ])),
+        travelTimeContainer()
       ]),
     );
   }
+}
+
+Widget _tripButtons(btnText, context) {
+  return Container(
+    margin: const EdgeInsets.all(20),
+    child: ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color.fromARGB(255, 45, 215, 245),
+        foregroundColor: Colors.black,
+      ),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      child: Text(btnText),
+    ),
+  );
+}
+
+travelTimeContainer() {
+  return Container(
+      decoration: BoxDecoration(
+        border: Border.all(width: 2, color: Colors.yellow),
+        color: Colors.black,
+      ),
+      child: Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+        Container(
+          margin: const EdgeInsets.all(20),
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+            color: Colors.black,
+          ),
+          child: const Text(
+            '01:30:25',
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+        )
+      ]));
 }
