b34d795fb12cbc36ee979b347f2d9e75e7c23cb6
Santanu Bhattacharya <santattech@gmail.com>
2023-07-02 16:35:58 +0530

    fix: render login screen when not login

--- lib/features/screens/splash/splash_screen.dart
+++ lib/features/screens/splash/splash_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:learningdart/features/screens/login/login_screen.dart';
 import 'package:learningdart/features/screens/splash/MyHomePage.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:learningdart/shared/app_secret.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({
@@ -20,7 +22,9 @@ class SplashScreen extends StatelessWidget {
               color: Colors.white,
               size: 50,
             ),
-            nextScreen: const MyHomePage(title: 'Home'),
+            nextScreen: AppSecret.accessToken == null
+                ? const LoginScreen()
+                : const MyHomePage(title: 'Home'),
             splashTransition: SplashTransition.fadeTransition,
             pageTransitionType: PageTransitionType.fade,
             backgroundColor: const Color.fromARGB(255, 169, 13, 13)));
